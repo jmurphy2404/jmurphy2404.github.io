ee9ab924a33bf0020eafa261e8062f46d885df4e
John Murphy <murf2073@gmail.com>
2018-01-09 11:46:48 -0500

    inital commit to deploy

---
+++ README.md
@@ -0,0 +1,3 @@
+# John Murphy's Developer Portfolio
+## NYCDA Sept 2017 Cohort
+Major Projects, Personal Information, Relevant Links
---
+++ main.js
@@ -0,0 +1,6 @@
+// document.addEventListener("DOMContentLoaded", function(){
+// 	document.getElementById("projectbox1").addEventListener("hover", function(){
+// 		document.getElementById("projectbox1").style.width = "100%";
+// 		document.getElementById("projectbox1").style.height = "600px";
+// 	})
+// })
