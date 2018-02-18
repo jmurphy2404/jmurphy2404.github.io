96745a576f2616c193bb0a5f16b0b6b016e328d9
John Murphy <murf2073@gmail.com>
2018-02-17 21:42:01 -0500

    click to hide contact and projects, added title to projects section

--- main.js
+++ main.js
@@ -1,6 +1,24 @@
-// document.addEventListener("DOMContentLoaded", function(){
-// 	document.getElementById("projectbox1").addEventListener("hover", function(){
-// 		document.getElementById("projectbox1").style.width = "100%";
-// 		document.getElementById("projectbox1").style.height = "600px";
-// 	})
-// })
+let contactHidden;
+let projectsHidden;
+
+document.addEventListener("DOMContentLoaded", function(){
+	document.getElementById("contact_title").addEventListener("click", function(){
+		if (!contactHidden) {
+			document.getElementById("contact").style.display = "none";
+			contactHidden = true;			
+		} else {
+			document.getElementById("contact").style.display = "block";
+			contactHidden = false;
+		}
+	})	
+	document.getElementById("projects_title").addEventListener("click", function(){
+		if (!projectsHidden) {
+			document.getElementById("projects").style.display = "none";
+			projectsHidden = true;			
+		} else {
+			document.getElementById("projects").style.display = "block";
+			projectsHidden = false;
+		}
+	})
+
+})
