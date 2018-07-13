8992aba409932577c30e76b26ec9f6301ee4c8f6
John Murphy <murf2073@gmail.com>
2018-07-13 14:27:33 -0400

    token revoked, added note to remind to replace when i find out how to work it into my portfolio

--- github.js
+++ github.js
@@ -1,3 +1,5 @@
+// replace token and find a workaround to hosting it on github bc it gets revoked immediately. tested and this code works though
+
 axios.get('https://api.github.com/users/jmurphy2404/repos', { headers: {"Authorization" : `Bearer afdcddb864e3e2d3f1b1f37ab5fe3a282d1dd933`} }).then((response) => {
 	console.log(response.data);
 });
